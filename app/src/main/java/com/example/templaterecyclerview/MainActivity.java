3538d80072f2515f0cbb212b15fa936d08d82796
DAM003 <berririsonia369@gmail.com>
2022-06-07 16:12:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/templaterecyclerview/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.templaterecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /* VAR GLOBALES*/
+    RecyclerView recyclerView;
+
+    private String[] stagiaires = new String[]{
+            "Nelson",
+            "Charbel",
+            "Hicham",
+            "Walid",
+            "Antoine",
+            "Nassir",
+            "Ben",
+            "Sonia",
+            "Ons",
+            "Anh Hoang",
+            "Samuel"
+    };
+
+
+String[] decriptions;
+
+int avatars[]={
+
+        R.drawable.stagiaire_01,
+        R.drawable.stagiaire_02,
+        R.drawable.stagiaire_03,
+        R.drawable.stagiaire_04,
+        R.drawable.stagiaire_05,
+        R.drawable.stagiaire_06,
+        R.drawable.stagiaire_07,
+        R.drawable.stagiaire_08,
+        R.drawable.stagiaire_09,
+        R.drawable.stagiaire_010,
+        R.drawable.stagiaire_011
+
+};
+/* Méthode d'initialisation des widgets*/
+    private void  intUI(){
+        recyclerView =findViewById(R.id.recyclerView);
+        // initiation des donnees 
+        decriptions =getResources().getStringArray(R.array.description);
+
+    }
+private void remplissageRecycler(){
+        RecyclerAdapter adapter=new RecyclerAdapter(this,stagiaires,decriptions,avatars);
+       recyclerView.setAdapter(adapter);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+
+    recyclerView.setLayoutManager(layoutManager);
+
+
+}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        intUI();
+        remplissageRecycler();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/templaterecyclerview/RecyclerAdapter.java
@@ -0,0 +1,79 @@
+package com.example.templaterecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+// Constructeur
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private Context context;
+    private String stagiaires[], descs[];
+    private int avatars[];
+
+    public RecyclerAdapter(Context context, String[] stagiaires, String[] descs, int[] avatars) {
+        this.context = context;
+        this.stagiaires = stagiaires;
+        this.descs = descs;
+        this.avatars = avatars;
+    }
+
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_recycler, parent, false);
+
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.stagiaire.setText(stagiaires[position]);
+        holder.description.setText(descs[position]);
+        holder.avatar.setImageResource(avatars[position]);
+
+holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(context,"stagiare:"+ stagiaires[position], Toast.LENGTH_LONG).show();
+    }
+});
+    }
+
+    @Override
+    public int getItemCount() {
+        return avatars.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView stagiaire, description;
+        ImageView avatar;
+        ConstraintLayout mainLayout;
+
+
+
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        /** Le contenu **/
+        stagiaire = itemView.findViewById(R.id.tvName);
+        description = itemView.findViewById(R.id.tvDesc);
+        avatar = itemView.findViewById(R.id.ivAvatar);
+
+        /** Le contenat**/
+        mainLayout = itemView.findViewById(R.id.mainLayout);
+    }
+}
+}
